7cca881d32d82ca3c3d8f533f618687e50f11d5c
Oktay Feim Faik <oktayfeimfaik99ies@gmail.com>
2020-02-13 18:59:38 +0100

    Paginas de tiendas y generos añadidas

--- app.js
+++ app.js
@@ -26,10 +26,63 @@ new Vue({
     computed: {
         gamesFilter: function() {
             var gameSearch = this.gameSearch;
-            return this.games.filter(function(el) {
+            return this.gamesFilter.filter(function(el) {
                 return el.name.toLowerCase().indexOf(gameSearch.toLowerCase()) !== -1;
             });
         }
-
     }
-})
+});
+
+new Vue ({
+    el: '#stores',
+    data() {
+        return {
+            storeSearch: "",
+            stores: []
+        }
+    },
+    created() {
+        axios({
+                "method": "GET",
+                "url": "https://rawg-video-games-database.p.rapidapi.com/stores",
+                "headers": {
+                    "content-type": "application/octet-stream",
+                    "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
+                    "x-rapidapi-key": "150c21a252msh32d2b3ca53cf3f1p13878ajsn24817a2fe099"
+                }
+        })
+        .then(response => {
+            this.stores = response.data.results;
+            console.log(response)
+        }).catch(e => {
+            console.log(e);
+        })
+    }
+});
+
+new Vue ({
+    el: '#genres',
+    data() {
+        return {
+            genreSearch: "",
+            genres: []
+        }
+    },
+    created() {
+        axios({
+                "method": "GET",
+                "url": "https://rawg-video-games-database.p.rapidapi.com/genres",
+                "headers": {
+                    "content-type": "application/octet-stream",
+                    "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
+                    "x-rapidapi-key": "150c21a252msh32d2b3ca53cf3f1p13878ajsn24817a2fe099"
+                }
+        })
+        .then(response => {
+            this.genres = response.data.results;
+            console.log(response)
+        }).catch(e => {
+            console.log(e);
+        })
+    }
+});
