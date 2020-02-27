64fd9aabe5ba31d8d14cf71dca6f027984666e8f
Oktay Feim Faik <oktayfeimfaik99ies@gmail.com>
2020-02-27 20:54:51 +0100

    Filtro realizado

--- js/app.js
+++ js/app.js
@@ -3,86 +3,91 @@ new Vue({
     data() {
         return {
             gameSearch: "",
-            games: []
+            games: [],
+            filteredGames: [],
+            existe: boolean = true
         }
     },
     created() {
         axios({
-                "method": "GET",
-                "url": "https://rawg-video-games-database.p.rapidapi.com/games",
-                "headers": {
-                    "content-type": "application/octet-stream",
-                    "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
-                    "x-rapidapi-key": "150c21a252msh32d2b3ca53cf3f1p13878ajsn24817a2fe099"
-                }
+            "method": "GET",
+            "url": "https://rawg-video-games-database.p.rapidapi.com/games",
+            "headers": {
+                "content-type": "application/octet-stream",
+                "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
+                "x-rapidapi-key": "150c21a252msh32d2b3ca53cf3f1p13878ajsn24817a2fe099"
+            }
         })
-        .then(response => {
-            this.games = response.data.results;
-            console.log(response)
-        }).catch(e => {
-            console.log(e);
-        })
+            .then(response => {
+                this.games = response.data.results;
+            }).catch(e => {
+            })
     },
-    computed: {
-        gamesFilter: function() {
-            var gameSearch = this.gameSearch;
-            return this.gamesFilter.filter(function(el) {
-                return el.name.toLowerCase().indexOf(gameSearch.toLowerCase()) !== -1;
+    methods: {
+        gamesFilter() {
+            this.filteredGames = [];
+            var gameSearch = this.gameSearch.trim();
+            var aux = [];
+            this.games.filter((el) => {
+                if (el.name.toLowerCase().includes(gameSearch.toLowerCase())) {
+                    aux.push(el);
+                }
             });
+            this.filteredGames = aux;
+
+            if(this.filteredGames == 0){
+                confirm('No hay resultados')
+            }
         }
     }
 });
 
-new Vue ({
+new Vue({
     el: '#stores',
     data() {
         return {
-            storeSearch: "",
             stores: []
         }
     },
     created() {
         axios({
-                "method": "GET",
-                "url": "https://rawg-video-games-database.p.rapidapi.com/stores",
-                "headers": {
-                    "content-type": "application/octet-stream",
-                    "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
-                    "x-rapidapi-key": "150c21a252msh32d2b3ca53cf3f1p13878ajsn24817a2fe099"
-                }
+            "method": "GET",
+            "url": "https://rawg-video-games-database.p.rapidapi.com/stores",
+            "headers": {
+                "content-type": "application/octet-stream",
+                "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
+                "x-rapidapi-key": "150c21a252msh32d2b3ca53cf3f1p13878ajsn24817a2fe099"
+            }
         })
-        .then(response => {
-            this.stores = response.data.results;
-            console.log(response)
-        }).catch(e => {
-            console.log(e);
-        })
+            .then(response => {
+                this.stores = response.data.results;
+            }).catch(e => {
+                console.log(e);
+            })
     }
 });
 
-new Vue ({
+new Vue({
     el: '#genres',
     data() {
         return {
-            genreSearch: "",
             genres: []
         }
     },
     created() {
         axios({
-                "method": "GET",
-                "url": "https://rawg-video-games-database.p.rapidapi.com/genres",
-                "headers": {
-                    "content-type": "application/octet-stream",
-                    "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
-                    "x-rapidapi-key": "150c21a252msh32d2b3ca53cf3f1p13878ajsn24817a2fe099"
-                }
+            "method": "GET",
+            "url": "https://rawg-video-games-database.p.rapidapi.com/genres",
+            "headers": {
+                "content-type": "application/octet-stream",
+                "x-rapidapi-host": "rawg-video-games-database.p.rapidapi.com",
+                "x-rapidapi-key": "150c21a252msh32d2b3ca53cf3f1p13878ajsn24817a2fe099"
+            }
         })
-        .then(response => {
-            this.genres = response.data.results;
-            console.log(response)
-        }).catch(e => {
-            console.log(e);
-        })
+            .then(response => {
+                this.genres = response.data.results;
+            }).catch(e => {
+                console.log(e);
+            })
     }
 });
